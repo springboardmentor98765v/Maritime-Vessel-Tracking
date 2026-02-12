630c9a1115cae0500ebda1e184a9d9511e0de444
sushantm1 <sushantm1100202@gmail.com>
2026-02-13 01:22:21 +0530

    updated reqirements.txt and settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 import os
