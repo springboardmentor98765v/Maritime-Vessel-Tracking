cccfc282ff1152b17b5731681741fd5b4a10a62c
sushantm1 <sushantm1100202@gmail.com>
2026-02-16 22:36:29 +0530

    sign-up and sign-in working

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,26 +1,4 @@
-// import axios from "axios";
 import api from './api'
-
-// export const loginUser = async (username, password) => {
-//   try {
-//     const response = await axios.post(
-//       "http://127.0.0.1:8000/auth/login/",
-//       {
-//         username: username,
-//         password: password,
-//       }
-//     );
-
-//     const { access, refresh } = response.data;
-
-//     localStorage.setItem("accessToken", access);
-//     localStorage.setItem("refreshToken", refresh);
-
-//     console.log("Login successful");
-//   } catch (error) {
-//     console.error("Login failed", error);
-//   }
-// };
 
 export const login = async (payload) => {
   const response = await api.post('/auth/login/', payload)
