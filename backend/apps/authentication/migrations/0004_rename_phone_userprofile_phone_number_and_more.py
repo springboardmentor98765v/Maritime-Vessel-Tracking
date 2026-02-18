0f5baf41bd759b3bdbcbc7d1860077c8eb905371
sushantm1 <sushantm1100202@gmail.com>
2026-02-18 19:24:18 +0530

    migratinn update

--- backend/apps/authentication/migrations/0004_rename_phone_userprofile_phone_number_and_more.py
+++ backend/apps/authentication/migrations/0004_rename_phone_userprofile_phone_number_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('authentication', '0003_alter_user_email'),
+        ('authentication', '0002_userprofile'),
     ]
 
     operations = [
